6025cb7f29890f0dd940daa4fbb8d68295c4a779
unknown <victor.tavares@cubotecnologia.com.br>
2019-08-14 11:57:23 -0300

    14/08

--- primeiroProjeto/primeiroprojeto/src/app/app.component.ts
+++ primeiroProjeto/primeiroprojeto/src/app/app.component.ts
@@ -6,14 +6,12 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title: string = 'Fabiano';
-    texto: string = 'fabs';
-    numero: number = 20;
-    verdadeirofalso: boolean = true;
-    lista = [5, 6, 7, 8, 9];
-    //lista1: Array<number>;
-    qualquercoisa: any;
-
-    variavel = `Varaivel numero com valor ${this.numero}`;
+    numero: number = 1;
+    texto: string = 'abhj';
+    booleano: boolean = true;
+    variavel = `Dados ${this.texto}`;
+    alerta() {
+        window.alert('ALERTA DO COMPONENTE DO PAI');
+    }
 }
 
--- primeiroProjeto/primeiroprojeto/src/app/segundo/segundo.component.ts
+++ primeiroProjeto/primeiroprojeto/src/app/segundo/segundo.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
+
     selector: 'app-segundo',
     templateUrl: './segundo.component.html'
 })
+
 export class SegundoComponent {
+    @Input() variavelRecebida;
 
+    @Input() naturalRecebida;
+    natural = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-
+    @Output() display = new EventEmitter();
 }
